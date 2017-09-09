33e90cb8ee551921d736ba0be3c772cfca5d8e3f
Rory Trunkhill <rtrunkhill@gmail.com>
2017-09-09 22:59:01 +0000

    cp24 complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-   resources :topics do
-     resources :posts, except: [:index]
-   end
-
-  get 'about' => 'welcome#about'
-  
-  root 'welcome#index'
-
+    resources :topics do
+        resources :posts, except: [:index]
+    end
+    
+    resources :users, only: [:new, :create]
+    
+    get 'about' => 'welcome#about'
+    
+    root 'welcome#index'
 end
