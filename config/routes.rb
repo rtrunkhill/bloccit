4afd61d577fda4bc0347275c097c9dc1dad9adcf
Rory Trunkhill <rtrunkhill@gmail.com>
2017-09-11 18:17:49 +0000

    cp25 assignment complete

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     end
     
     resources :users, only: [:new, :create]
+    post 'users/confrim' => 'users#confirm'
     
     get 'about' => 'welcome#about'
     
