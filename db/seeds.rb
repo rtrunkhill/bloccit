8b7e56733d109ea5d42fc7e7f65d90b3e7f81305
Rory Trunkhill <rtrunkhill@gmail.com>
2017-09-12 20:44:08 +0000

    cp27 complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,13 @@
  require 'random_data'
+ 
+ 5.times do
+   User.create!(
+   name:     RandomData.random_name,
+   email:    RandomData.random_email,
+   password: RandomData.random_sentence
+   )
+ end
+ users = User.all
 
  15.times do
    Topic.create!(
@@ -10,6 +19,7 @@
  
  50.times do
    Post.create!(
+     user:   users.sample,
      topic:  topics.sample,
      title:  RandomData.random_sentence,
      body:   RandomData.random_paragraph
@@ -24,7 +34,14 @@
    )
  end
  
+ user = User.first
+ user.update_attributes!(
+   email: 'skepts@gmail.com', 
+   password: 'helloworld'
+ )
+ 
  puts "Seed finished"
+ puts "#{User.count} users created"
  puts "#{Topic.count} topics created"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
