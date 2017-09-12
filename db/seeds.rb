129644d21f3f883d6c8f5d02c5263e9dd4d301dc
Rory Trunkhill <rtrunkhill@gmail.com>
2017-09-12 23:20:11 +0000

    cp28-complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -34,9 +34,18 @@
    )
  end
  
- user = User.first
- user.update_attributes!(
-   email: 'skepts@gmail.com', 
+ # Create an admin user
+ admin = User.create!(
+   name:     'Admin User',
+   email:    'admin@example.com',
+   password: 'helloworld',
+   role:     'admin'
+ )
+ 
+ # Create a member
+ member = User.create!(
+   name:     'Member User',
+   email:    'member@example.com',
    password: 'helloworld'
  )
  
